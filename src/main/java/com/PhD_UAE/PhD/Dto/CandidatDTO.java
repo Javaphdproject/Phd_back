9641294c4d542480783e53c5f8a0208192873cb2
FzBerradi <brdfatimazahra@gmail.com>
2024-10-27 12:15:52 +0100

    Update Candidat DTO+ENTITY

--- src/main/java/com/PhD_UAE/PhD/Dto/CandidatDTO.java
+++ src/main/java/com/PhD_UAE/PhD/Dto/CandidatDTO.java
@@ -3,6 +3,7 @@ package com.PhD_UAE.PhD.Dto;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.List;
 
 
 @Data
@@ -15,11 +16,72 @@ public class CandidatDTO {
     private String cne;
     private String pays;
     private String photo;
-    private String diplomeObtenu;
-    private String etablissementPrecedent;
     private boolean fonctionnaire;
     private int idEntretien;
     private Long idBourse;
     private int idRendezVous;
     private int idSujet;
+
+    // Informations supplémentaires
+    private String situationFamiliale;
+    private String nationalite;
+    private String paysNaissance;
+    private String provinceNaissance;
+    private String lieuNaissance;
+    private String sexe;
+    private String codePostal;
+    private String adressePermanente;
+    private String emailInstitutionnel;
+    private boolean handicape;
+    private String professionPere;
+    private String professionMere;
+    private String Candidatprofession;
+    private String organismeEmployeur;
+
+    // Baccalauréat
+    private String nomLycee;
+    private String paysBac;
+    private String provinceBac;
+    private String filiereBac;
+    private String anneeObtentionBac;
+    private String mentionBac;
+    private double moyenneBac;
+
+    // Licence
+    private String nomUniversiteLicence;
+    private String paysLicence;
+    private String specialiteLicence;
+    private String anneeObtentionLicence;
+    private String mentionLicence;
+    private double moyenneLicence;
+
+    // Master
+    private String nomUniversiteMaster;
+    private String paysMaster;
+    private String statutMaster;
+    private String specialiteMaster;
+    private String anneeObtentionMaster;
+    private String mentionMaster;
+    private double moyenneMaster;
+
+    // Langues
+    private List<String> langues;
+    private List<String> niveauxLangues;
+
+    // Expérience professionnelle
+    private boolean experienceProfessionnelle;
+    private List<String> organisme;
+    private List<String> fonctions;
+    private List<String> secteurs;
+    private List<Date> duDates;
+    private List<Date> auDates;
+
+    // Documents scannés
+    private String baccalaureatScanne;
+    private String licenceScanne;
+    private String masterScanne;
+    private String releveNoteMasterScanne;
+    private String releveNoteLicenceScanne;
+    private String carteNationaleScanne;
+    private String cvScanne;
 }

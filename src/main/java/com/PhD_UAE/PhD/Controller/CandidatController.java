551182794c64ba60c5d6561761e9a656028637c8
FzBerradi <brdfatimazahra@gmail.com>
2024-10-29 07:31:09 +0100

    update service + controller candidat

--- src/main/java/com/PhD_UAE/PhD/Controller/CandidatController.java
+++ src/main/java/com/PhD_UAE/PhD/Controller/CandidatController.java
@@ -1,4 +1,4 @@
-package com.PhD_UAE.PhD.Controller;
+/*package com.PhD_UAE.PhD.Controller;
 
 import com.PhD_UAE.PhD.Dto.CandidatDTO;
 import com.PhD_UAE.PhD.Service.CandidatService;
@@ -17,4 +17,26 @@ public class CandidatController {
     public String createCandidat(@RequestBody CandidatDTO candidatDTO, @PathVariable Long userId) {
         return candidatService.createCandidat(candidatDTO, userId);
     }
+}*/
+package com.PhD_UAE.PhD.Controller;
+
+import com.PhD_UAE.PhD.Dto.CandidatDTO;
+import com.PhD_UAE.PhD.Service.CandidatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/phd/candidat")
+public class CandidatController {
+
+    @Autowired
+    private CandidatService candidatService;
+
+    @PostMapping("/create/{userId}")
+    public String createCandidat(@Validated @RequestBody CandidatDTO candidatDTO, @PathVariable Long userId) {
+        return candidatService.createCandidat(candidatDTO, userId);
+    }
 }
+
--- src/main/java/com/PhD_UAE/PhD/Service/CandidatService.java
+++ src/main/java/com/PhD_UAE/PhD/Service/CandidatService.java
@@ -44,7 +44,54 @@ public class CandidatService {
         candidat.setCandidatprofession(candidatDTO.getCandidatprofession());
         candidat.setOrganismeEmployeur(candidatDTO.getOrganismeEmployeur());
 
-        // Save the Candidat
+        // Baccalauréat
+        candidat.setNomLycee(candidatDTO.getNomLycee());
+        candidat.setPaysBac(candidatDTO.getPaysBac());
+        candidat.setProvinceBac(candidatDTO.getProvinceBac());
+        candidat.setFiliereBac(candidatDTO.getFiliereBac());
+        candidat.setAnneeObtentionBac(candidatDTO.getAnneeObtentionBac());
+        candidat.setMentionBac(candidatDTO.getMentionBac());
+        candidat.setMoyenneBac(candidatDTO.getMoyenneBac());
+
+        // Licence
+        candidat.setNomUniversiteLicence(candidatDTO.getNomUniversiteLicence());
+        candidat.setPaysLicence(candidatDTO.getPaysLicence());
+        candidat.setSpecialiteLicence(candidatDTO.getSpecialiteLicence());
+        candidat.setAnneeObtentionLicence(candidatDTO.getAnneeObtentionLicence());
+        candidat.setMentionLicence(candidatDTO.getMentionLicence());
+        candidat.setMoyenneLicence(candidatDTO.getMoyenneLicence());
+
+        // Master
+        candidat.setNomUniversiteMaster(candidatDTO.getNomUniversiteMaster());
+        candidat.setPaysMaster(candidatDTO.getPaysMaster());
+        candidat.setStatutMaster(candidatDTO.getStatutMaster());
+        candidat.setSpecialiteMaster(candidatDTO.getSpecialiteMaster());
+        candidat.setAnneeObtentionMaster(candidatDTO.getAnneeObtentionMaster());
+        candidat.setMentionMaster(candidatDTO.getMentionMaster());
+        candidat.setMoyenneMaster(candidatDTO.getMoyenneMaster());
+
+        // Langues
+        candidat.setLangues(candidatDTO.getLangues());
+        candidat.setNiveauxLangues(candidatDTO.getNiveauxLangues());
+
+        // Expérience professionnelle
+        candidat.setExperienceProfessionnelle(candidatDTO.isExperienceProfessionnelle());
+        candidat.setOrganisme(candidatDTO.getOrganisme());
+        candidat.setFonctions(candidatDTO.getFonctions());
+        candidat.setSecteurs(candidatDTO.getSecteurs());
+        candidat.setDuDates(candidatDTO.getDuDates());
+        candidat.setAuDates(candidatDTO.getAuDates());
+
+        // Documents scannés
+        candidat.setBaccalaureatScanne(candidatDTO.getBaccalaureatScanne());
+        candidat.setLicenceScanne(candidatDTO.getLicenceScanne());
+        candidat.setMasterScanne(candidatDTO.getMasterScanne());
+        candidat.setReleveNoteMasterScanne(candidatDTO.getReleveNoteMasterScanne());
+        candidat.setReleveNoteLicenceScanne(candidatDTO.getReleveNoteLicenceScanne());
+        candidat.setCarteNationaleScanne(candidatDTO.getCarteNationaleScanne());
+        candidat.setCvScanne(candidatDTO.getCvScanne());
+
+        // Sauvegarder le Candidat
         candidatRepository.save(candidat);
         return "Candidat created successfully";
     }

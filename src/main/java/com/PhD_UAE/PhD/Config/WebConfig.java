422a573597e0ece717fa864fac75a3e1532ef5bb
hayat <life1234vida@gmail.com>
2024-11-05 03:33:40 +0100

    dd

--- src/main/java/com/PhD_UAE/PhD/Config/WebConfig.java
+++ src/main/java/com/PhD_UAE/PhD/Config/WebConfig.java
@@ -1,19 +1,2 @@
-package com.PhD_UAE.PhD.Config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // Allow CORS for all endpoints
-                .allowedOrigins("http://localhost:4200")  // Allow requests from Angular frontend
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
+package com.PhD_UAE.PhD.Config;public class WebConfig {
 }
